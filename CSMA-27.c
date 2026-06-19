5b8f583839e5d3ac47a4e5ebdeb3006617833b72
Cauamts103 <darkliomm@gmail.com>
2026-06-19 02:40:02 -0300

    Create CSMA-27.c

---
+++ CSMA-27.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    printf("\n**************************************************************************************** ");
+    printf("\n* Aluno: Cauã da Silva de Matos Alves - RA:0027026 *");
+    printf("\n* Programa CSMA-27 - Tabuada de um Número * ");
+    printf("\n**************************************************************************************** \n");
+ 
+   int num, i;
+
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    for(i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+
+    return 0;
+}
