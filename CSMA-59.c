f6113fd689ef37be088093a84fbe32789b87bd4a
Cauamts103 <darkliomm@gmail.com>
2026-06-19 19:48:26 -0300

    Update CSMA-59.c

--- CSMA-59.c
+++ CSMA-59.c
@@ -6,7 +6,7 @@ int main() {
     setlocale(LC_ALL, "Portuguese");
     printf("\n**************************************************************************************** ");
     printf("\n* Aluno: Cauã da Silva de Matos Alves - RA:0027026 *");
-    printf("\n* Programa CSMA- 58 - O Assistente de Direção (GPS Sem Mapa ) ");
+    printf("\n* Programa CSMA- 59 - O Assistente de Direção (GPS Sem Mapa ) ");
     printf("\n**************************************************************************************** \n");
 
     char direcao;
