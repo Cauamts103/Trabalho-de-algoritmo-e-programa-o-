f8f53b8f534a8843c9401f9ed98a29f84f826b31
Cauamts103 <darkliomm@gmail.com>
2026-06-19 02:25:11 -0300

    Create CSMA-22.c

---
+++ CSMA-22.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    printf("\n**************************************************************************************** ");
+    printf("\n* Aluno: Cauã da Silva de Matos Alves - RA:0027026 *");
+    printf("\n* Programa CSMA-22 - Par ou Ímpar * ");
+    printf("\n**************************************************************************************** \n");
+ 
+int main() {
+    int numero;
+
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
+
+    if (numero % 2 == 0) {
+        printf("O numero é PAR.\n");
+    } else {
+        printf("O numero e IMPAR.\n");
+    }
+
+    return 0;
+}
