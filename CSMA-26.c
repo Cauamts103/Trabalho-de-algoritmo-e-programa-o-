9dcad53a5602ebe51450cef99a6778c85882cb44
Cauamts103 <darkliomm@gmail.com>
2026-06-19 02:37:47 -0300

    Create CSMA-26.c

---
+++ CSMA-26.c
@@ -0,0 +1,20 @@
+ude<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    printf("\n**************************************************************************************** ");
+    printf("\n* Aluno: Cauã da Silva de Matos Alves - RA:0027026 *");
+    printf("\n* Programa CSMA-26 - Contar de 1 a 10 * ");
+    printf("\n**************************************************************************************** \n");
+ 
+int main() {
+    int i;
+
+    for(i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
